65d76ddf092ebb345a46c006c90bf84ceed27d52
Mattias Petter Johansson <mpj@mpj.me>
2012-03-26 16:54:05 +0200

    Finally nailed the edge case.

--- lottery.js
+++ lottery.js
@@ -13,7 +13,7 @@ exports.canWeGetTickets = function (opts, cb) {
 	} else {
 		// Less friends than tickets. 
 		// This means that we need to win more than one time.
-		var winsNeeded = friends / tickets;
+		var winsNeeded = Math.ceil( friends / tickets);
 		if (winsNeeded == friends) {
 			// This works in the case where all friends need to win.
 			// We simply calculate the chance if winning for
@@ -48,20 +48,81 @@ exports.canWeGetTickets = function (opts, cb) {
 			En person kan inte vinna två gånger.
 			Vad är oddsen för att vi vinner?
 
-			ODDSET FÖR ATT 4 PERSONER VINNER  ^antalet gånger vi behöver vinna?
-			
+			HUr stor är chansen att en person vinner två gånger?
+			Kompensera för att chansen att vinna är dubbel
+
 			*/
 
-			var winProbability = 1
-			for (var i=0;i<winsNeeded;i++) {
-				winProbability *= chanceOfWinning(entrants-i,winners-i,friends-i)
+			// Hur stor är chansen att vårt lag vinner två gånger ?
+
+
+			var cases = module.exports.filterCases(
+				module.exports.generateCases(friends), winsNeeded)
+			winProbability = 0;
+			for (var i=0;i<cases.length;i++) {
+				winProbability += 
+					module.exports.calculateCaseProbability(entrants, winners,cases[i])
 			}
+
+			winProbability = winProbability*2
 	       
 		}
 
 		cb(null, winProbability.toFixed(10))
 	}
 
+}
+
+module.exports.calculateCaseProbability = function(entrants, winners, caseArr) {
+	var prob = 1
+	for (var i=0;i<caseArr.length;i++) {
+		var isWin = !!caseArr[i];
+		if(isWin) // Skip over case unless we are winner
+			prob *= (winners-i) / (entrants-i)
+	}
+	return prob;
+}
+
+
+module.exports.generateCases = function(friends) {
+
+	var cases = [[0], [1]];
+
+	for(var j=0;j<friends;j++) {
+		var l=cases.length;
+		for(var i=0;i<l;i++) {
+			var cw = cases[i].slice(0);
+			var cl = cases[i].slice(0);
+			cw.push(1)
+			cl.push(0)
+			cases.push(cw)
+			cases.push(cl)
+		}
+		var cleanedCases = []
+		for(var i=0;i<cases.length;i++) 
+			if (cases[i].length == j+1)
+				cleanedCases.push(cases[i])
+		cases = cleanedCases
+	
+	}
+	return cases;
+}
+
+module.exports.filterCases = function(cases, winsNeeded) {
+	var filteredCases = []
+	for(var i=0;i<cases.length;i++) 
+		if(winCount(cases[i]) >= winsNeeded)
+			filteredCases.push(cases[i])
+	return filteredCases;
+}
+
+function winCount(caseArr) {
+	var count = 0
+	for(var i=0;i<caseArr.length;i++) {
+		if (caseArr[i])
+			count++
+	}
+	return count
 }
 
 // Calculate the chance of a single person winning a draw, 
--- spec/lottery_spec.js
+++ spec/lottery_spec.js
@@ -61,31 +61,156 @@ vows.describe('canWeWin').addBatch({
             friends: 6
         }),
 
-    '10 entrants, 6 friends, 6 winners, 1 tickets each': 
+    '80 entrants, 4 friends, 4 winners, 2 tickets each': 
         createGetTickersContext({
             entrants: 80,
-            winners: 8,
+            winners: 4,
             tickets: 2,
             friends: 4
         }),
 
     /*
-        Utmaningen här är att vi har fyra vänner på oss
-        att vinna TVÅ gånger. 
+        Calculate the probability for all possible cases and combine.
 
-        Försök till lösning: Räkna ut risken för att 
-        FÖRLORA två gånger med fyra vänner och invertera det.
+        The number of chars = friends (i.e. the chances we have in the draw)
+        We need to generate the probability of all cases where our group 
+        wins enough times to be able to buy tickets for everyone.
 
 
+        {
+            entrants: 80,
+            winners: 4,
+            tickets: 2,
+            friends: 4
+        }
 
+        Simulated prob: 0.01099723
+
+        WIN cases:
+            4 wins (1 case):
+            WWWW = (4/80)*(3/79)*(2/78)*(1/77)  = .000000632 
+            
+            3 wins:
+            WWWL = (4/80)*(3/79)*(2/78)         = .000048685
+            WWLW = (4/80)*(3/79)       *(1/77)  = .000024659
+            WLWW = (4/80)       *(2/78)*(1/77)  = .00001665
+            LWWW =        (3/79)*(2/78)*(1/77)  = .000012646
+
+            2 wins
+            WWLL = (4/80)*(3/79)                = .001898734
+            WLWL = (4/80)       *(2/78)         = .001282051
+            WLLW = (4/80)*              (1/77)  = .000649351
+            LWWL =        (3/79)*(2/78)         = .00097371
+            LWLW =        (3/79)       *(1/77)  = .000493178
+            LLWW =               (2/78)*(1/77)  = .000333
+
+            .000000632+
+            .000048685+
+            .000024659+
+            .00001665+
+            .000012646+
+            .001898734+
+            .001282051+
+            .000649351+
+            .00097371+
+            .000493178+
+            .000333=.005733296
+
+            // hmmm
+
+            .005733296 * 2 is correct. I wonder if this is 
+            the number of wins needed or just the double?
+
+        {
+            entrants: 80,
+            winners: 4,
+            tickets: 2,
+            friends: 5
+        }
+
+        Simulated prob: 0.0004931
+
+        // 3 wins needed
+
+        4 wins (1 case):
+        WWWW = (4/80)*(3/79)*(2/78)*(1/77)  = .000000632 
+        
+        3 wins:
+        WWWL = (4/80)*(3/79)*(2/78)         = .000048685
+        WWLW = (4/80)*(3/79)       *(1/77)  = .000024659
+        WLWW = (4/80)       *(2/78)*(1/77)  = .00001665
+        LWWW =        (3/79)*(2/78)*(1/77)  = .000012646
+
+
+        .000000632+
+        .000048685+
+        .000024659+
+        .00001665+
+        .000012646=.000103272        
+        
     */
 
+    'calculateCaseProbability 1': {
+        topic: function() {
+           return lottery.calculateCaseProbability(80, 4, [1,1,1,1])
+        },
+        'should calculate correct probability': function(prob) {
+            assert.equal(prob, 6.322791132917715e-7)
+        }
+    },
 
+    'calculateCaseProbability 2': {
+        topic: function() {
+           return lottery.calculateCaseProbability(80, 4, [1,1,0,1])
+        },
+        'should calculate correct probability': function(prob) {
+            assert.equal(prob, 0.000024658885418379094)
+        }
+    },
 
+    'generateCases': {
+        topic: function() {
+            return lottery.generateCases(4)
+        },
 
+        'correct': function(err, cases) {
+            if(err) throw err;
+            
+            assert.equal(cases.length, 16)
+            assert.isTrue(arrayContainsArray(cases, [1,1,1,1]))
+            assert.isTrue(arrayContainsArray(cases, [1,1,1,0]))
+            assert.isTrue(arrayContainsArray(cases, [1,1,0,0]))
+            assert.isTrue(arrayContainsArray(cases, [1,0,0,0]))
+            assert.isTrue(arrayContainsArray(cases, [1,0,1,0]))
+            assert.isTrue(arrayContainsArray(cases, [0,0,1,0]))
+            assert.isTrue(arrayContainsArray(cases, [0,0,0,0]))
+            assert.isTrue(arrayContainsArray(cases, [0,1,0,0]))
 
+        } 
 
+    },
 
+    'filterCases': {
+        topic: function() {
+            return lottery.filterCases([
+                [1,0,1,0,1,1],
+                [0,0,1,0,1,1],
+                [0,0,0,0,1,1],
+                [0,0,0,1,1,1],
+                [1,0,0,0,0,1],
+                [1,1,0,1,0,1]
+            ], 3)
+        },
+
+        'should return all cases with 3 or more wins': function(filtered) {
+            assert.equal(filtered.length, 4)
+            assert.isTrue(arrayContainsArray(filtered, [1,0,1,0,1,1]))
+            assert.isTrue(arrayContainsArray(filtered, [0,0,1,0,1,1]))
+            assert.isTrue(arrayContainsArray(filtered, [1,0,1,0,1,1]))
+            assert.isTrue(arrayContainsArray(filtered, [1,1,0,1,0,1]))
+        }
+
+    }
 
 
 }).export(module); 
@@ -145,3 +270,18 @@ function simulateDrawing(opts, cb) {
 function randomInRange(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function arrayContainsArray(arrContainer, arrFind) {
+    for(var i=0;i<arrContainer.length;i++)
+        if (arrayMatchesArray(arrContainer[i], arrFind))
+            return true;
+    return false;
+}
+
+function arrayMatchesArray(left, right) {
+    for(var i=0;i<left.length;i++)
+        if(left[i] != right[i])
+            return false;
+    return true;
+}
+
