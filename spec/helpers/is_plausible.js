6da6420186912d0872012b94b8af9ff931041419
Mattias Petter Johansson <mpj@mpj.me>
2012-03-31 16:35:38 +0200

    Documented and clarified isPlausible

--- spec/helpers/is_plausible.js
+++ spec/helpers/is_plausible.js
@@ -1,30 +1,44 @@
-/* 
+
+
+/** 
+ *  isPlausible  
+ *
+ *  A simple test that simulates an actual lottery an insane amount of 
+ *  times and compares that to a given probability to see if it's plausible.
+ *   
+ *  @probability {number} The probability that we want to check if it's plausible,
+ *  in the form of a number between 0 and 1.
+ *  
+ *  @opts {object} Lottery options - accepts exactly the same options as 
+ *  canWeGetTickets accepts. 
+ *  
+ *  @return {boolean} True if @probability is a plausible winning probability
+ *  given @opts, otherwise false;
+ *  
  */
-
-
-function isPlausible(result, opts, cb) {
-    var simulatedProbability = simulateDrawing(opts, function(simulationResult) {
+function isPlausible(probability, opts, callback) {
+    _simulateLottery(opts, function(simulationResult) {
     	var errorMargin = 0.001
-    	cb(
-    	   (result < simulationResult + errorMargin) &&
-           (result > simulationResult - errorMargin)
+    	callback(
+    	   (probability < simulationResult + errorMargin) &&
+           (probability > simulationResult - errorMargin)
         )
     })
 }
 
-function simulateDrawing(opts, cb) {
+function _simulateLottery(opts, cb) {
     
     var entrants = opts.entrants;
     var winners  = opts.winners ? opts.winners : 1;
     var friends  = opts.friends ? opts.friends : 1;
     var tickets  = opts.tickets ? opts.tickets : 99;
 
-    var drawings = 1000000;
+    var simulations = 1000000;
     var wins = 0;
-    for(var i=0;i<drawings;i++) {
+    for(var i=0;i<simulations;i++) {
         var winningFriends = 0
         for(var j=0;j<winners;j++) {
-            var rand = randomInRange(1, entrants-j)
+            var rand = _randomInRange(1, entrants-j)
             var weWonADrawing = ((friends-winningFriends) >= rand)
             if(weWonADrawing)
                 winningFriends++
@@ -33,10 +47,10 @@ function simulateDrawing(opts, cb) {
             wins++;
         }
     }
-    cb(wins/drawings);
+    cb(wins/simulations);
 }
 
-function randomInRange(min, max){
+function _randomInRange(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
