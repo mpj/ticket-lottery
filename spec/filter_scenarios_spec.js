75a68fb02382839d0bbb18223b96697c4022ab34
Mattias Petter Johansson <mpj@mpj.me>
2012-03-31 16:54:36 +0200

    Documented and clarified filterScenarios.js

--- spec/filter_scenarios_spec.js
+++ spec/filter_scenarios_spec.js
@@ -4,24 +4,39 @@ var vows                = require('vows'),
 
 vows.describe('filterScenarios').addBatch({
 
-    'filterScenarios': {
+    'filterScenarios (mininum 3 wins)': {
         topic: function() {
             return filterScenarios([
-                [1,0,1,0,1,1],
-                [0,0,1,0,1,1],
-                [0,0,0,0,1,1],
-                [0,0,0,1,1,1],
-                [1,0,0,0,0,1],
-                [1,1,0,1,0,1]
-            ], 3)
+                [ 0,1,0,1,0 ], // 2 wins
+                [ 0,1,1,1,0 ], // 3 wins
+                [ 1,1,0,1,1 ], // 4 wins
+                [ 0,0,0,0,1 ], // 1 win
+            ], 3, 99)
         },
 
         'Should return all scenarios with 3 or more wins': function(filtered) {
-            assert.equal( filtered.length, 4 );
-            assertContainsArray( filtered, [1,0,1,0,1,1] );
-            assertContainsArray( filtered, [0,0,1,0,1,1] );
-            assertContainsArray( filtered, [1,0,1,0,1,1] );
-            assertContainsArray( filtered, [1,1,0,1,0,1] );
+            assert.equal( filtered.length, 2 );
+            assertContainsArray( filtered, [ 0,1,1,1,0 ] );
+            assertContainsArray( filtered, [ 1,1,0,1,1 ] );
+        }
+
+    },
+
+    'filterScenarios (min 2 wins, max 3 wins)': {
+        topic: function() {
+            return filterScenarios([
+                [ 1,0,0,0 ],
+                [ 1,1,0,0 ],
+                [ 1,1,1,0 ],
+                [ 1,1,1,1 ],
+
+            ], 2, 3)
+        },
+
+        'Should filter out scenarios outside range': function(filtered) {
+            assert.equal( filtered.length, 2 );
+            assertContainsArray( filtered, [ 1,1,0,0 ] );
+            assertContainsArray( filtered, [ 1,1,1,0 ] );
         }
 
     }
