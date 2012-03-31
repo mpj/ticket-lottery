8bb1f7f2e46d32b9a51987b5dcbd8630a5bec889
Mattias Petter Johansson <mpj@mpj.me>
2012-03-31 16:10:14 +0200

    scenario_probability_spec.js

--- spec/lottery_spec.js
+++ spec/lottery_spec.js
@@ -2,9 +2,7 @@ var vows                = require('vows'),
     assert              = require('assert'),
     arrayContainsArray  = require('./helpers/array_contains_array'),
     lotterySimulator    = require('./helpers/lottery_simulator'),
-    generateScenarios   = require('../lib/generate_scenarios'),
     filterScenarios     = require('../lib/filter_scenarios'),
-    scenarioProbability = require('../lib/scenario_probability')
     lottery             = require('../lib/lottery');
 
 vows.describe('canWeWin').addBatch({
@@ -74,59 +72,6 @@ vows.describe('canWeWin').addBatch({
             tickets: 3,
             friends: 9
         }),
-
-
-    'calculateScenarioProbability 1': {
-    	
-    	topic: function() {
-    		var p1 = scenarioProbability([ 1, 0 ], 2, 100)
-    		var p2 = scenarioProbability([ 1, 1 ], 2, 100)
-    		var p3 = scenarioProbability([ 0, 1 ], 2, 100)
-    		var p4 = scenarioProbability([ 0, 0 ], 2, 100)
-    		return p1+p2+p3+p4
-    	},
-
-    	'should be 1': function(prob) {
-    		assert.equal(prob, 1)
-    	}
-    },
-
-    'calculateScenarioProbability 2': {
-    	
-    	topic: function() {
-    		var scenarios10Winners = generateScenarios(10);
-    		var totalProb = 0;
-    		for (var i=0;i<scenarios10Winners.length;i++) {
-    			var s = scenarios10Winners[i];
-    			totalProb += scenarioProbability(s, 2, 100);
-    		}
-
-    		return totalProb;
-    	},
-
-    	'should be 1': function(prob) {
-    		assert.equal(prob, 1);
-    	}
-    },
-
-    'calculateScenarioProbability 3': {
-    	
-    	topic: function() {
-    		var scenarios10Winners = generateScenarios(10);
-			var scenariosWhereWeGetEnoughTickets = filterScenarios(scenarios10Winners, 1, 2);
-    		var totalProb = 0;
-    		for (var i=0;i<scenariosWhereWeGetEnoughTickets.length;i++) {
-    			var s = scenariosWhereWeGetEnoughTickets[i];
-    			totalProb += scenarioProbability(s, 2, 100);
-    		}
-
-    		return totalProb;
-    	},
-
-    	'should be correct': function(prob) {
-    		assert.equal(prob, 0.19090909090909097);
-    	}
-    },
 
     'filterScenarios': {
         topic: function() {
---
+++ spec/scenario_probability_spec.js
@@ -0,0 +1,65 @@
+var vows                = require('vows'),
+    assert              = require('assert'),
+    lotterySimulator    = require('./helpers/lottery_simulator'),
+    generateScenarios   = require('../lib/generate_scenarios'),
+    filterScenarios     = require('../lib/filter_scenarios'),
+    scenarioProbability = require('../lib/scenario_probability')
+    lottery             = require('../lib/lottery');
+
+vows.describe('calculateScenarioProbability').addBatch({
+
+    'calculateScenarioProbability 1': {
+    	
+    	topic: function() {
+    		var p1 = scenarioProbability([ 1, 0 ], 2, 100)
+    		var p2 = scenarioProbability([ 1, 1 ], 2, 100)
+    		var p3 = scenarioProbability([ 0, 1 ], 2, 100)
+    		var p4 = scenarioProbability([ 0, 0 ], 2, 100)
+    		return p1+p2+p3+p4
+    	},
+
+    	'should be 1': function(prob) {
+    		assert.equal(prob, 1)
+    	}
+    },
+
+    'calculateScenarioProbability 2': {
+    	
+    	topic: function() {
+    		var scenarios10Winners = generateScenarios(10);
+    		var totalProb = 0;
+    		for (var i=0;i<scenarios10Winners.length;i++) {
+    			var s = scenarios10Winners[i];
+    			totalProb += scenarioProbability(s, 2, 100);
+    		}
+
+    		return totalProb;
+    	},
+
+    	'should be 1': function(prob) {
+    		assert.equal(prob, 1);
+    	}
+    },
+
+    'calculateScenarioProbability 3': {
+    	
+    	topic: function() {
+    		var scenarios10Winners = generateScenarios(10);
+			var scenariosWhereWeGetEnoughTickets = filterScenarios(scenarios10Winners, 1, 2);
+    		var totalProb = 0;
+    		for (var i=0;i<scenariosWhereWeGetEnoughTickets.length;i++) {
+    			var s = scenariosWhereWeGetEnoughTickets[i];
+    			totalProb += scenarioProbability(s, 2, 100);
+    		}
+
+    		return totalProb;
+    	},
+
+    	'should be 0.19090909090909097': function(prob) {
+    		assert.equal(prob, 0.19090909090909097);
+    	}
+    }
+
+}).export(module); 
+
+
