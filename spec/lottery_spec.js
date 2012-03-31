0df7de5abf0dac345a85121f2d69ce1b8db57399
Mattias Petter Johansson <mpj@mpj.me>
2012-03-31 15:59:17 +0200

    Moved arrayContainsArray to separate file and added lots of semicolons.

--- spec/lottery_spec.js
+++ spec/lottery_spec.js
@@ -1,5 +1,6 @@
 var vows                = require('vows'),
     assert              = require('assert'),
+    arrayContainsArray  = require('./helpers/array_contains_array'),
     lotterySimulator    = require('./helpers/lottery_simulator'),
     generateScenarios   = require('../lib/generate_scenarios'),
     filterScenarios     = require('../lib/filter_scenarios'),
@@ -27,11 +28,11 @@ vows.describe('canWeWin').addBatch({
         }),
 
     '10 entrants, 1 friends, 10 winners (infinite tickets)': 
-    createGetTickersContext({
-        entrants: 10,
-        winners: 10,
-        friends: 1
-    }),
+        createGetTickersContext({
+            entrants: 10,
+            winners: 10,
+            friends: 1
+        }),
 
     '100 entrants, 2 friends, 10 winners, 1 tickets each': 
         createGetTickersContext({
@@ -93,58 +94,57 @@ vows.describe('canWeWin').addBatch({
     'calculateScenarioProbability 2': {
     	
     	topic: function() {
-    		var scenarios10Winners = generateScenarios(10)
-    		var totalProb = 0
+    		var scenarios10Winners = generateScenarios(10);
+    		var totalProb = 0;
     		for (var i=0;i<scenarios10Winners.length;i++) {
-    			var s = scenarios10Winners[i]
-    			totalProb += scenarioProbability(s, 2, 100)
+    			var s = scenarios10Winners[i];
+    			totalProb += scenarioProbability(s, 2, 100);
     		}
 
-    		return totalProb
+    		return totalProb;
     	},
 
     	'should be 1': function(prob) {
-    		assert.equal(prob, 1)
+    		assert.equal(prob, 1);
     	}
     },
 
     'calculateScenarioProbability 3': {
     	
     	topic: function() {
-    		var scenarios10Winners = generateScenarios(10)
-			var scenariosWhereWeGetEnoughTickets = filterScenarios(scenarios10Winners, 1, 2)
-    		var totalProb = 0
+    		var scenarios10Winners = generateScenarios(10);
+			var scenariosWhereWeGetEnoughTickets = filterScenarios(scenarios10Winners, 1, 2);
+    		var totalProb = 0;
     		for (var i=0;i<scenariosWhereWeGetEnoughTickets.length;i++) {
-    			var s = scenariosWhereWeGetEnoughTickets[i]
-    			totalProb += scenarioProbability(s, 2, 100)
+    			var s = scenariosWhereWeGetEnoughTickets[i];
+    			totalProb += scenarioProbability(s, 2, 100);
     		}
 
-    		return totalProb
+    		return totalProb;
     	},
 
     	'should be correct': function(prob) {
-    		assert.equal(prob, 0.19090909090909097)
+    		assert.equal(prob, 0.19090909090909097);
     	}
     },
 
     'generateScenarios': {
         topic: function() {
-            return generateScenarios(4)
+            return generateScenarios(4);
         },
 
         'correct': function(err, scenarios) {
             if(err) throw err;
             
-            assert.equal(scenarios.length, 16)
-            assert.isTrue(arrayContainsArray(scenarios, [1,1,1,1]))
-            assert.isTrue(arrayContainsArray(scenarios, [1,1,1,0]))
-            assert.isTrue(arrayContainsArray(scenarios, [1,1,0,0]))
-            assert.isTrue(arrayContainsArray(scenarios, [1,0,0,0]))
-            assert.isTrue(arrayContainsArray(scenarios, [1,0,1,0]))
-            assert.isTrue(arrayContainsArray(scenarios, [0,0,1,0]))
-            assert.isTrue(arrayContainsArray(scenarios, [0,0,0,0]))
-            assert.isTrue(arrayContainsArray(scenarios, [0,1,0,0]))
-
+            assert.equal(scenarios.length, 16);
+            assert.isTrue(arrayContainsArray(scenarios, [1,1,1,1]));
+            assert.isTrue(arrayContainsArray(scenarios, [1,1,1,0]));
+            assert.isTrue(arrayContainsArray(scenarios, [1,1,0,0]));
+            assert.isTrue(arrayContainsArray(scenarios, [1,0,0,0]));
+            assert.isTrue(arrayContainsArray(scenarios, [1,0,1,0]));
+            assert.isTrue(arrayContainsArray(scenarios, [0,0,1,0]));
+            assert.isTrue(arrayContainsArray(scenarios, [0,0,0,0]));
+            assert.isTrue(arrayContainsArray(scenarios, [0,1,0,0]));
         } 
 
     },
@@ -162,15 +162,14 @@ vows.describe('canWeWin').addBatch({
         },
 
         'should return all scenarios with 3 or more wins': function(filtered) {
-            assert.equal(filtered.length, 4)
-            assert.isTrue(arrayContainsArray(filtered, [1,0,1,0,1,1]))
-            assert.isTrue(arrayContainsArray(filtered, [0,0,1,0,1,1]))
-            assert.isTrue(arrayContainsArray(filtered, [1,0,1,0,1,1]))
-            assert.isTrue(arrayContainsArray(filtered, [1,1,0,1,0,1]))
+            assert.equal(filtered.length, 4);
+            assert.isTrue(arrayContainsArray(filtered, [1,0,1,0,1,1]));
+            assert.isTrue(arrayContainsArray(filtered, [0,0,1,0,1,1]));
+            assert.isTrue(arrayContainsArray(filtered, [1,0,1,0,1,1]));
+            assert.isTrue(arrayContainsArray(filtered, [1,1,0,1,0,1]));
         }
 
     }
-
 
 }).export(module); 
 
@@ -179,27 +178,13 @@ function createGetTickersContext(opts) {
         topic: function() {
         	var cb = this.callback
             lottery.canWeGetTickets(opts, function(err, probability) {
-            	lotterySimulator.isPlausible(probability, opts, cb)
+            	lotterySimulator.isPlausible(probability, opts, cb);
             });
         },
 
         'returns plausible probability': function (isPlausibleResult) {
-            assert.isTrue(isPlausibleResult)
+            assert.isTrue(isPlausibleResult);
         }
     }
-}
-
-function arrayContainsArray(arrContainer, arrFind) {
-    for(var i=0;i<arrContainer.length;i++)
-        if (arrayMatchesArray(arrContainer[i], arrFind))
-            return true;
-    return false;
-}
-
-function arrayMatchesArray(left, right) {
-    for(var i=0;i<left.length;i++)
-        if(left[i] != right[i])
-            return false;
-    return true;
 }
 
